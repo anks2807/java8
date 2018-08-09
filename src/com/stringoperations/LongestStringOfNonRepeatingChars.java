d88106265476dd6be4fdc8747bc1ef6bab7fea17
Ankit Singhal <ankitsinghal2807@gmail.com>
2018-08-10 00:03:05 +0530

    longest non repeating chars program

---
+++ src/com/stringoperations/LongestStringOfNonRepeatingChars.java
@@ -0,0 +1,39 @@
+package com.stringoperations;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LongestStringOfNonRepeatingChars {
+
+	public static void main(String[] args) {
+		longestStringOfNonRepeatingChars("Javaconceptoftheday");
+	}
+
+	public static void longestStringOfNonRepeatingChars(String string){
+		Map<Character, Integer> nonRepMapChars = new LinkedHashMap<Character, Integer>();
+		int longestNonRepeatingChars = 0;
+		String longestNonRepeatingSubStr = null;
+		char[] charArr = string.toLowerCase().toCharArray();
+
+		for(int i=0 ; i < charArr.length;i++){
+			if(!nonRepMapChars.containsKey(charArr[i])) {
+				nonRepMapChars.put(charArr[i], i);
+			} else{
+				i= nonRepMapChars.get(charArr[i]);
+				nonRepMapChars.clear();
+			}
+
+			if(nonRepMapChars.size() > longestNonRepeatingChars) {
+				longestNonRepeatingChars = nonRepMapChars.size();
+				longestNonRepeatingSubStr = nonRepMapChars.keySet().toString();
+			}
+		}
+
+		System.out.println("longest number of non repeating substring"+ longestNonRepeatingChars);
+		System.out.println("longest non repeating substring"+ longestNonRepeatingSubStr);
+
+	}
+
+}
+
+
