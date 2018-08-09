7eac7cca20abcd82ceaddc762d7006c37c9db61c
Ankit Singhal <ankit.singhal@exlservice.com>
2018-08-09 05:44:53 -0700

    swapping two string without using third variable

--- src/com/stringoperations/AnagramString.java
+++ src/com/stringoperations/AnagramString.java
@@ -8,6 +8,7 @@ public class AnagramString {
 		System.out.println(isAnagram("Mother In Law".replaceAll("\\s", "").toLowerCase(), "Hitler Woman".replaceAll("\\s", "").toLowerCase()));
 		System.out.println(isAnagram("School MASTER".replaceAll("\\s", "").toLowerCase(), "The ClassROOM".replaceAll("\\s", "").toLowerCase()));
 		System.out.println(isAnagram("ASTRONOMERS".replaceAll("\\s", "").toLowerCase(), "NO MORaE STARS".replaceAll("\\s", "").toLowerCase()));
+		swap("Java", "J2EE");
 	}
 	
 	public static boolean isAnagram(String str1 , String str2) {
@@ -22,6 +23,18 @@ public class AnagramString {
 			}
 			return true;
 		}
-	} 
+	}
+	
+	public static void swap(String s1 , String s2) {
+		if(s1 !=null && s2 !=null) {
+
+			System.out.println("Before Swapping "+ s1+","+ s2);
+			s1 = s1+s2;
+			s2 = s1.substring(0, s1.length()- s2.length());
+			s1 = s1.substring(s2.length());
+			
+			System.out.println("After Swapping "+ s1+","+ s2);
+		}
+	}
 
 }
